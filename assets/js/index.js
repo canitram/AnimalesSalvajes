a0cc8bb89738a468ec94923da2bb13a76c4d95cf
canitram <ma.covarrubias.a@gmail.com>
2021-09-13 18:30:06 -0400

    Muestra tarjeta agregadas desde el formulario

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,7 +54,11 @@ const recargaTabla = () =>{
    tabla.insertAdjacentHTML('beforeend', `<div class="card ml-2" style="width: 12rem; background-color: #767777">
    <img src="${nuevaInfo.getImg()}" class="card-img-top img-fluid" alt="...">
    <div class="card-body">
-   <button>Play</button>
+   <a href="#" style="text-decoration: none; color= ##343A40">
+   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+   <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+   </svg>
+   </a>
    </div>
  </div>`);
 }
